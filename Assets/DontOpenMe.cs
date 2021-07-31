50a00b34bab9ad11b66606d2559e71dee25d9ea2
Simsimhan Chobo <jun32820@gmail.com>
2021-08-01 03:58:05 +0900

    뭐했더라

--- Assets/DontOpenMe.cs
+++ Assets/DontOpenMe.cs
@@ -12,27 +12,39 @@ public class DontOpenMe : MonoBehaviour
 
     void Update()
     {
-        //Don't run this code
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.C) && Input.GetKey(KeyCode.L) && Input.GetKey(KeyCode.V) && Input.GetKey(KeyCode.Alpha1) && Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.I) && Input.GetKey(KeyCode.P) && Input.GetKeyDown(KeyCode.Return))
+        if (!Text)
         {
-            SoundManager.StopAll(SoundType.All, false);
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Don't open me and play");
-            MainCamera.Camera.enabled = false;
-            SdjkSystem.sdjkSystem.gameObject.SetActive(false);
+            //Don't run this code
+            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.C) && Input.GetKey(KeyCode.L) && Input.GetKey(KeyCode.V) && Input.GetKey(KeyCode.Alpha1) && Input.GetKey(KeyCode.M) && Input.GetKey(KeyCode.I) && Input.GetKey(KeyCode.P) && Input.GetKeyDown(KeyCode.Return))
+            {
+                SoundManager.StopAll(SoundType.All, false);
+                MainCamera.Camera.enabled = false;
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Don't open me and play");
+                MainMenu.mainMenu.enabled = false;
+                return;
+            }
+            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.C) && Input.GetKey(KeyCode.L) && Input.GetKey(KeyCode.V) && Input.GetKey(KeyCode.Alpha1) && Input.GetKey(KeyCode.P) && Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.SceneLoading("Play Mode");
+                GameManager.Level = "extra/School Live! OP";
+                MainMenu.mainMenu.enabled = false;
+                return;
+            }
         }
-        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.C) && Input.GetKey(KeyCode.L) && Input.GetKey(KeyCode.V) && Input.GetKey(KeyCode.Alpha1) && Input.GetKey(KeyCode.P) && Input.GetKeyDown(KeyCode.Return))
+        else
         {
-            SceneManager.SceneLoading("Play Mode");
-            GameManager.Level = "extra/School Live! OP";
-            MainMenu.mainMenu.enabled = false;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.SceneLoading("Main Menu");
+                MainCamera.Camera.enabled = true;
+                SdjkSystem.sdjkSystem.gameObject.SetActive(true);
+            }
         }
     }
 
-    IEnumerator Start()
+    void Start()
     {
-        yield return new WaitForSeconds(1);
-
         if (Text)
-            gameObject.SetActive(false);
+            SdjkSystem.sdjkSystem.gameObject.SetActive(false);
     }
 }
